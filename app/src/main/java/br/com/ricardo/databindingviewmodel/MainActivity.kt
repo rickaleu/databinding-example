39c180f5adb0a3ffe5445ceb1ce0cf6340c578eb
Ricardo Sousa <ti.ricardo.sousa@gmail.com>
2020-09-15 17:42:19 -0300

    Add dataBinding.

--- app/src/main/java/br/com/ricardo/databindingviewmodel/MainActivity.kt
+++ app/src/main/java/br/com/ricardo/databindingviewmodel/MainActivity.kt
@@ -2,10 +2,19 @@ package br.com.ricardo.databindingviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import br.com.ricardo.databindingviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
     }
 }
---
+++ app/src/main/java/br/com/ricardo/databindingviewmodel/MainViewModel.kt
@@ -0,0 +1,24 @@
+package br.com.ricardo.databindingviewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private var number = 1
+
+    private val _counter = MutableLiveData<String>()
+    val counter : LiveData<String>
+        get() = _counter
+
+
+    init {
+        _counter.value = number.toString()
+    }
+
+    fun clickMe() {
+        _counter.value = (++number).toString()
+    }
+
+}
